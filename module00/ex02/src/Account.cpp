b7ab4cb686fc0bace3ce8a39e542e4f9fd6deb40
louisnfr <louis.raffin@gmail.com>
2022-03-08 18:40:21 +0100

    cpp

--- module00/ex02/src/Account.cpp
+++ module00/ex02/src/Account.cpp
@@ -6,42 +6,78 @@
 /*   By: lraffin <lraffin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/08 00:09:51 by lraffin           #+#    #+#             */
-/*   Updated: 2022/03/08 00:23:38 by lraffin          ###   ########.fr       */
+/*   Updated: 2022/03/08 18:05:35 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <iomanip>
+#include <ctime>
 #include "Account.hpp"
 
 Account::Account(int initial_deposit)
 {
 	this->_amount = initial_deposit;
+	_nbAccounts++;
+	_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex;
+	std::cout << ";amount:" << this->_amount;
+	std::cout << ";created" << std::endl;
 	return ;
 }
 
 Account::~Account(void)
 {
+	_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex;
+	std::cout << ";amount:" << this->_amount;
+	std::cout << ";closed" << std::endl;
 	return ;
 }
 
 void	Account::makeDeposit(int deposit)
 {
+	(void)deposit;
 }
 
 bool	Account::makeWithdrawal(int withdrawal)
 {
+	(void)withdrawal;
+	return (false);
 }
 
 // consts tutorial
 // https://elearning.intra.42.fr/notions/piscine-c-d00-c-basics/subnotions/piscine-c-d00-c-basics-const/videos/const-c0f10448-8fd3-4a06-83ed-abcce68c18a1
 
 int		Account::checkAmount(void) const
-
+{
+	return (0);
 }
 
 void	Account::displayStatus(void) const
 {
+	std::cout << "status" << std::endl;
 }
 
 void	Account::displayAccountsInfos(void)
 {
+	_displayTimestamp();
+	std::cout << "accounts:" << _nbAccounts;
+	std::cout << ";total:";
+}
+
+void	Account::_displayTimestamp(void)
+{
+	time_t	timestamp = time(NULL);
+	tm		*now = localtime(&timestamp);
+
+	std::cout << "[";
+	std::cout << now->tm_year + 1900;
+	std::cout << std::setw(2) << std::setfill('0') << now->tm_mon + 1;
+	std::cout << std::setw(2) << std::setfill('0') << now->tm_mday;
+	std::cout << "_";
+	std::cout << std::setw(2) << std::setfill('0') << now->tm_hour;
+	std::cout << std::setw(2) << std::setfill('0') << now->tm_min;
+	std::cout << std::setw(2) << std::setfill('0') << now->tm_sec;
+	std::cout << "] ";
 }
