462d8196351364817f37754f16fecf62116c13b6
louisnfr <louis.raffin@gmail.com>
2022-03-09 17:25:11 +0100

    Weapon done

---
+++ module01/ex03/src/Weapon.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Weapon.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lraffin <lraffin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/09 17:21:17 by lraffin           #+#    #+#             */
+/*   Updated: 2022/03/09 17:24:46 by lraffin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Weapon.hpp"
+
+Weapon::Weapon(void)
+{
+	return ;
+}
+
+Weapon::~Weapon(void)
+{
+	return ;
+}
+
+const std::string	&Weapon::getType(void)
+{
+	return (this->_type);
+}
+
+void	Weapon::setType(std::string type)
+{
+	this->_type = type;
+}
