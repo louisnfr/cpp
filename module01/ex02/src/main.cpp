cb741fe567c6f8e520c769eb8f12b0a22a726fa9
louisnfr <louis.raffin@gmail.com>
2022-03-09 17:05:14 +0100

    address of stringPTR fixed

--- module01/ex02/src/main.cpp
+++ module01/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lraffin <lraffin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 15:45:07 by lraffin           #+#    #+#             */
-/*   Updated: 2022/03/09 16:05:29 by lraffin          ###   ########.fr       */
+/*   Updated: 2022/03/09 17:04:32 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	main(void)
 	std::cout << "addr. of string:\t" << &string << std::endl;
 	std::cout << std::endl;
 	std::cout << "value of stringPTR:\t" << *stringPTR << std::endl;
-	std::cout << "addr. of stringPTR:\t" << &stringPTR << std::endl;
+	std::cout << "addr. of stringPTR:\t" << stringPTR << std::endl;
 	std::cout << std::endl;
 	std::cout << "value of stringREF:\t" << stringREF << std::endl;
 	std::cout << "addr. of stringREF:\t" << &stringREF << std::endl;
