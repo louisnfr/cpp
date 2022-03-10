e42726c1725a7f5f4dc047c321ce48e3475b6747
Louis RAFFIN <lraffin@e1r10p9.42.fr>
2022-03-10 17:54:52 +0100

    cpp (make push)

--- module01/ex04/src/main.cpp
+++ module01/ex04/src/main.cpp
@@ -6,18 +6,31 @@
 /*   By: lraffin <lraffin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 18:47:41 by lraffin           #+#    #+#             */
-/*   Updated: 2022/03/09 18:50:16 by lraffin          ###   ########.fr       */
+/*   Updated: 2022/03/10 17:54:42 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
 
-int	main(void)
+int	main(int ac, char **av)
 {
-	std::string	filename;
-	std::string	s1;
-	std::string	s2;
+	std::fstream	file;
+	std::string		s1;
 
-	return (0);
+	(void)ac;
+	if (ac != 4)
+		return (EXIT_FAILURE);
+	// s1 = "";
+	file.open(av[1], std::fstream::in);
+	while (s1 != "louis")
+	{
+		file >> s1;
+		std::cout << s1 << std::endl;
+	}
+	file.close();
+	file.open((std::string)av[1] + ".replace", std::fstream::out);
+	file << s1 << std::endl;
+	file.close();
+	return (EXIT_SUCCESS);
 }
