9197c10127785b15692602fbbc19da2551b3ffec
Louis Raffin <louis.raffin@gmail.com>
2022-03-22 00:35:43 +0100

    weird leak to be freed

--- module05/ex00/inc/Bureaucrat.hpp
+++ module05/ex00/inc/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: lraffin <lraffin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 22:40:59 by lraffin           #+#    #+#             */
-/*   Updated: 2022/03/21 22:40:59 by lraffin          ###   ########.fr       */
+/*   Updated: 2022/03/22 00:22:24 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,32 @@
 # define __BUREAUCRAT_HPP__
 
 # include <iostream>
+# include <exception>
 
 class Bureaucrat
 {
 	public:
-		Bureaucrat(void);
+		Bureaucrat(std::string const &name, unsigned int grade);
 		Bureaucrat(Bureaucrat const &src);
 		Bureaucrat &operator=(Bureaucrat const &rhs);
 		~Bureaucrat(void);
-		int	getVar(void) const;
+		std::string const	getName(void) const;
+		unsigned int		getGrade(void) const;
+		void				promote(void);
+		void				demote(void);
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
 	private:
-		int	_var;
+		std::string const	_name;
+		unsigned int		_grade;
 };
 
 std::ostream	&operator<<(std::ostream &o, Bureaucrat const &i);
