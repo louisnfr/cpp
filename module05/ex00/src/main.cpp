86d9f1f50b7d4451394a58cabc785cf9cd1a1ff0
Louis Raffin <louis.raffin@gmail.com>
2022-03-22 01:44:16 +0100

    Done, leak to be checked #13

--- module05/ex00/src/main.cpp
+++ module05/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lraffin <lraffin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 22:40:26 by lraffin           #+#    #+#             */
-/*   Updated: 2022/03/22 00:27:16 by lraffin          ###   ########.fr       */
+/*   Updated: 2022/03/22 01:39:34 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,54 +14,54 @@
 
 int	main(void)
 {
-	// Bureaucrat	tommy("Thomas", 1);
-	// Bureaucrat	tom(tommy);
-	// Bureaucrat	arthur("Arthur", 2);
-	// Bureaucrat	john("John", 149);
+	Bureaucrat	tommy("Thomas", 1);
+	Bureaucrat	tom(tommy);
+	Bureaucrat	arthur("Arthur", 2);
+	Bureaucrat	john("John", 149);
 
-	// std::cout << std::endl;
+	std::cout << std::endl;
 
-	// std::cout << tommy << std::endl;
-	// std::cout << arthur << std::endl;
-	// std::cout << john << std::endl;
+	std::cout << tommy << std::endl;
+	std::cout << arthur << std::endl;
+	std::cout << john << std::endl;
 
-	// std::cout << std::endl;
+	std::cout << std::endl;
 
-	// arthur.promote();
-	// std::cout << arthur << std::endl;
-	// john.demote();
-	// std::cout << john << std::endl;
+	arthur.promote();
+	std::cout << arthur << std::endl;
+	john.demote();
+	std::cout << john << std::endl;
 
-	// std::cout << std::endl;
+	std::cout << std::endl;
 
-	// try
-	// {
-	// 	arthur.promote();
-	// }
-	// catch (std::exception & e)
-	// {
-	// 	std::cerr << e.what() << std::endl;
-	// }
+	try
+	{
+		arthur.promote();
+	}
+	catch (std::exception & e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
 
-	// try
-	// {
-	// 	john.demote();
-	// }
-	// catch (std::exception & e)
-	// {
-	// 	std::cerr << e.what() << std::endl;
-	// }
+	try
+	{
+		john.demote();
+	}
+	catch (std::exception & e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
 
-	// std::cout << std::endl;
+	std::cout << std::endl;
 
-	// try
-	// {
-	// 	Bureaucrat	polly("Polly", 0);
-	// }
-	// catch (std::exception & e)
-	// {
-	// 	std::cerr << e.what() << std::endl;
-	// }
+	try
+	{
+		Bureaucrat	polly("Polly", 0);
+	}
+	catch (std::exception & e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
 
 	try
 	{
@@ -71,7 +71,8 @@ int	main(void)
 	{
 		std::cout << e.what() << std::endl;
 	}
-	// std::cout << std::endl;
+	
+	std::cout << std::endl;
 
 	return (0);
 }
