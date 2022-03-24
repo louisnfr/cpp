275272c7e7edcc1cea506c5909287764367b597c
louisnfr <louis.raffin@gmail.com>
2022-03-24 18:33:35 +0100

    Corrected algorithm

--- module00/ex00/src/megaphone.cpp
+++ module00/ex00/src/megaphone.cpp
@@ -6,12 +6,20 @@
 /*   By: lraffin <lraffin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/03 02:02:40 by lraffin           #+#    #+#             */
-/*   Updated: 2022/03/08 18:19:31 by lraffin          ###   ########.fr       */
+/*   Updated: 2022/03/24 18:28:45 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <algorithm>
 #include <iostream>
+
+void	strToUpper(char *str)
+{
+	int	i;
+
+	i = -1;
+	while (str[++i])
+		str[i] = toupper(str[i]);
+}
 
 int	main(int ac, char **av)
 {
@@ -19,9 +27,8 @@ int	main(int ac, char **av)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	for (int i = 1; av[i]; i++)
 	{
-		std::string str = av[i];
-		std::transform(str.begin(), str.end(), str.begin(), toupper);
-		std::cout << str;
+		strToUpper(av[i]);
+		std::cout << av[i];
 	}
 	std::cout << std::endl;
 	return (0);
