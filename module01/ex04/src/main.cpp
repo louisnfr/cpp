a4b7f3720d517f57d601f0c212517133ae289d4b
louisnfr <louis.raffin@gmail.com>
2022-03-25 14:56:27 +0100

    Fix error management

--- module01/ex04/src/main.cpp
+++ module01/ex04/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lraffin <lraffin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 18:47:41 by lraffin           #+#    #+#             */
-/*   Updated: 2022/03/25 14:41:23 by lraffin          ###   ########.fr       */
+/*   Updated: 2022/03/25 14:54:51 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,19 @@ static void	exit_error(char const *error_message)
 {
 	std::cerr << "Error: " << error_message << std::endl;
 	exit (EXIT_FAILURE);
+}
+
+static void	is_empty(std::ifstream &infile)
+{
+	std::string		line;
+
+	std::getline(infile, line);
+	if (line.empty())
+	{
+		infile.close();
+		exit_error("file empty");
+	}
+	infile.close();
 }
 
 static void	replace_line(std::string *line, std::string s1, std::string s2)
@@ -34,13 +47,20 @@ static void	replace_line(std::string *line, std::string s1, std::string s2)
 	}
 }
 
-static void	search_file(std::ifstream &infile, std::ofstream &outfile,
+static void	search_file(std::ifstream &infile, std::string replace,
 						std::string s1, std::string s2)
 {
-	std::string	line;
+	std::string		line;
+	std::ofstream	outfile;
 
 	if (s1.empty())
+	{
+		infile.close();
 		exit_error("s1 is empty");
+	}
+	outfile.open(replace.c_str());
+	if (outfile.fail())
+		exit_error("failed to open file");
 	while (!infile.eof())
 	{
 		std::getline(infile, line);
@@ -50,12 +70,11 @@ static void	search_file(std::ifstream &infile, std::ofstream &outfile,
 			replace_line(&line, s1, s2);
 		outfile << line << std::endl;
 	}
+	outfile.close();
 }
-
 int	main(int ac, char **av)
 {
 	std::ifstream	infile;
-	std::ofstream	outfile;
 	std::string		replace;
 
 	if (ac != 4)
@@ -63,12 +82,10 @@ int	main(int ac, char **av)
 	infile.open(av[1]);
 	if (infile.fail())
 		exit_error("failed to open file");
+	is_empty(infile);
+	infile.open(av[1]);
 	replace = (std::string)av[1] + ".replace";
-	outfile.open(replace.c_str());
-	if (outfile.fail())
-		exit_error("failed to open file");
-	search_file(infile, outfile, av[2], av[3]);
+	search_file(infile, replace, av[2], av[3]);
 	infile.close();
-	outfile.close();
 	return (0);
 }
