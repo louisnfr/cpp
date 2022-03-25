1112dad98f915aecfb158b623cea26daa813e276
louisnfr <louis.raffin@gmail.com>
2022-03-25 14:22:51 +0100

    Improved newZombie function

--- module01/ex00/src/newZombie.cpp
+++ module01/ex00/src/newZombie.cpp
@@ -6,7 +6,7 @@
 /*   By: lraffin <lraffin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 02:12:27 by lraffin           #+#    #+#             */
-/*   Updated: 2022/03/09 02:52:48 by lraffin          ###   ########.fr       */
+/*   Updated: 2022/03/25 14:19:32 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,5 @@
 
 Zombie	*newZombie(std::string name)
 {
-	Zombie *zombie;
-
-	zombie = new Zombie(name);
-	return (zombie);
+	return (new Zombie(name));
 }
