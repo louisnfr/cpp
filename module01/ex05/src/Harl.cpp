ba37f09b2b3b225e24221fcdf9126c5bef4162fd
louisnfr <louis.raffin@gmail.com>
2022-03-25 14:59:50 +0100

    Added underscore for private members

--- module01/ex05/src/Harl.cpp
+++ module01/ex05/src/Harl.cpp
@@ -22,28 +22,28 @@ Harl::~Harl(void)
 	return ;
 }
 
-void	Harl::debug(void)
+void	Harl::_debug(void)
 {
 	std::cout << "I love having extra bacon for my ";
 	std::cout << "7XL-double-cheese-triple-pickle-specialketchup burger. ";
 	std::cout << "I really do !" << std::endl;
 }
 
-void	Harl::info(void)
+void	Harl::_info(void)
 {
 	std::cout << "I cannot believe adding extra bacon costs more money. ";
 	std::cout << "You didn't put enough bacon in my burger ! ";
 	std::cout << "If you did, I wouldn't be asking for more !" << std::endl;
 }
 
-void	Harl::warning(void)
+void	Harl::_warning(void)
 {
 	std::cout << "I think I deserve to have some extra bacon for free. ";
 	std::cout << "I've been coming for years whereas you started working here ";
 	std::cout << "since last month." << std::endl;
 }
 
-void	Harl::error(void)
+void	Harl::_error(void)
 {
 	std::cout << "This is unacceptable ! I want to speak to the manager now.";
 	std::cout << std::endl;
@@ -54,8 +54,8 @@ void	Harl::complain(std::string level)
 	Harl		karen;
 	int			i;
 	std::string	str[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
-	void		(Harl::*func[4])(void) = {&Harl::debug, &Harl::info,
-										&Harl::warning, &Harl::error};
+	void		(Harl::*func[4])(void) = {&Harl::_debug, &Harl::_info,
+										&Harl::_warning, &Harl::_error};
 
 	i = 0;
 	while (i < 4)
