434ad97b38e412df1e19b44f482ba269f635d577
Louis Raffin <louis.raffin@gmail.com>
2022-03-29 23:47:57 +0200

    Fix #24 and improved error management

--- module01/ex04/src/main.cpp
+++ module01/ex04/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lraffin <lraffin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 18:47:41 by lraffin           #+#    #+#             */
-/*   Updated: 2022/03/25 14:54:51 by lraffin          ###   ########.fr       */
+/*   Updated: 2022/03/29 23:41:55 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,28 @@ static void	is_empty(std::ifstream &infile)
 	std::string		line;
 
 	std::getline(infile, line);
-	if (line.empty())
+	if (infile.eof())
 	{
 		infile.close();
 		exit_error("file empty");
+	}
+	infile.close();
+}
+
+static void	check_errors(int ac, char **av)
+{
+	std::ifstream	infile;
+
+	if (ac != 4)
+		exit_error("bad arguments");
+	infile.open(av[1]);
+	if (infile.fail())
+		exit_error("failed to open file");
+	is_empty(infile);
+	if (static_cast<std::string>(av[2]).empty())
+	{
+		infile.close();
+		exit_error("s1 is empty");
 	}
 	infile.close();
 }
@@ -53,22 +71,16 @@ static void	search_file(std::ifstream &infile, std::string replace,
 	std::string		line;
 	std::ofstream	outfile;
 
-	if (s1.empty())
-	{
-		infile.close();
-		exit_error("s1 is empty");
-	}
 	outfile.open(replace.c_str());
 	if (outfile.fail())
 		exit_error("failed to open file");
 	while (!infile.eof())
 	{
 		std::getline(infile, line);
-		if (line.empty())
-			break ;
 		if (line.find(s1) != std::string::npos)
 			replace_line(&line, s1, s2);
-		outfile << line << std::endl;
+		if (!infile.eof())
+			outfile << line << std::endl;
 	}
 	outfile.close();
 }
@@ -77,14 +89,9 @@ int	main(int ac, char **av)
 	std::ifstream	infile;
 	std::string		replace;
 
-	if (ac != 4)
-		exit_error("bad arguments");
+	check_errors(ac, av);
 	infile.open(av[1]);
-	if (infile.fail())
-		exit_error("failed to open file");
-	is_empty(infile);
-	infile.open(av[1]);
-	replace = (std::string)av[1] + ".replace";
+	replace = static_cast<std::string>(av[1]) + ".replace";
 	search_file(infile, replace, av[2], av[3]);
 	infile.close();
 	return (0);
