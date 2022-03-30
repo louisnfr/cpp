f7f3dfa5f625edd38da426817034a92b9d89d746
louisnfr <louis.raffin@gmail.com>
2022-03-30 15:44:49 +0200

    cpp (make push)

---
+++ module08/ex00/inc/easyfind.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lraffin <lraffin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/30 15:26:32 by lraffin           #+#    #+#             */
+/*   Updated: 2022/03/30 15:34:48 by lraffin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __EASYFIND_HPP__
+# define __EASYFIND_HPP__
+
+# include <iostream>
+# include <algorithm>
+
+template<typename T>
+T	easyfind(T *container, int n)
+{
+
+}
+
+#endif
