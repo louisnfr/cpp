949a030e2ea4ca707d545c8310b459c41ac12fdb
Louis Raffin <louis.raffin@gmail.com>
2022-04-03 19:12:29 +0200

    Done

--- module08/ex02/inc/MutantStack.hpp
+++ module08/ex02/inc/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: lraffin <lraffin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/03 18:23:45 by lraffin           #+#    #+#             */
-/*   Updated: 2022/04/03 18:23:45 by lraffin          ###   ########.fr       */
+/*   Updated: 2022/04/03 19:02:58 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,31 @@
 # define __MUTANTSTACK_HPP__
 
 # include <iostream>
+# include <stack>
+# include <list>
 
-class MutantStack
+template <typename T>
+class MutantStack : public std::stack<T>
 {
 	public:
-		MutantStack(void);
-		MutantStack(MutantStack const &src);
-		MutantStack &operator=(MutantStack const &rhs);
-		~MutantStack(void);
-		int	getVar(void) const;
-	private:
-		int	_var;
-};
+		MutantStack(void) {};
+		MutantStack(MutantStack const &src) {
+			*this = src;
+		};
+		MutantStack &operator=(MutantStack const &rhs) {
+			this->c = rhs.c;
+			return (*this);
+		};
+		~MutantStack(void) {};
 
-std::ostream	&operator<<(std::ostream &o, MutantStack const &i);
+		typedef typename std::stack<T>::container_type::iterator iterator;
+
+		iterator begin() {
+			return this->c.begin();
+		};
+		iterator end() {
+			return this->c.end();
+		};
+};
 
 #endif
--- module08/ex02/src/main.cpp
+++ module08/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lraffin <lraffin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/03 18:23:47 by lraffin           #+#    #+#             */
-/*   Updated: 2022/04/03 18:25:32 by lraffin          ###   ########.fr       */
+/*   Updated: 2022/04/03 19:11:51 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,42 +14,57 @@
 
 int	main(void)
 {
-	// MutantStack<int> mstack;
+	std::cout << "USING MUTANT STACK" << std::endl;
+	{
+		MutantStack<int>	mstack;
 
-	// mstack.push(5);
-	// mstack.push(17);
+		if (mstack.empty())
+			std::cout << "empty" << std::endl;
+		mstack.push(5);
+		mstack.push(17);
+		std::cout << mstack.top() << std::endl;
+		mstack.pop();
+		std::cout << mstack.size() << std::endl;
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+		mstack.push(0);
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::stack<int> s(mstack);
+	}
+	std::cout << std::endl << "USING LIST" << std::endl;
+	{
+		std::list<int>	mlist;
 
-	// std::cout << mstack.top() << std::endl;
-
-	// mstack.pop();
-
-	// std::cout << mstack.size() << std::endl;
-
-	// mstack.push(3);
-	// mstack.push(5);
-	// mstack.push(737);
-	// //[...]
-	// mstack.push(0);
-
-	// MutantStack<int>::iterator it = mstack.begin();
-	// MutantStack<int>::iterator ite = mstack.end();
-	// ++it;
-	// --it;
-
-	// while (it != ite)
-	// {
-	// 	std::cout << *it << std::endl;
-	// 	++it;
-	// }
-
-	// std::stack<int> s(mstack);
-
+		if (mlist.empty())
+			std::cout << "empty" << std::endl;
+		mlist.push_back(5);
+		mlist.push_back(17);
+		std::cout << mlist.back() << std::endl;
+		mlist.pop_back();
+		std::cout << mlist.size() << std::endl;
+		mlist.push_back(3);
+		mlist.push_back(5);
+		mlist.push_back(737);
+		mlist.push_back(0);
+		std::list<int>::iterator it = mlist.begin();
+		std::list<int>::iterator ite = mlist.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::list<int> s(mlist);
+	}
 	return (0);
 }
-
-/*
-Si vous l’exécutez une première fois avec votre MutantStack, puis une seconde fois en
-remplaçant la MutantStack, par exemple par std::list, les deux résultats devront être
-identiques. Bien sûr, lorsque vous testez cet exemple avec un autre container, modifiez le
-code avec les fonctions membres correspondantes (push() peut devenir push_back()).
-*/
