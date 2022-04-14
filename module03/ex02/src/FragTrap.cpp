642b46203c22b73082186fc414a1ff56c20b8130
louisnfr <louis.raffin@gmail.com>
2022-04-14 14:42:07 +0200

    Fix copy constructor Frag and Scav

--- module03/ex02/src/FragTrap.cpp
+++ module03/ex02/src/FragTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: lraffin <lraffin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/17 16:51:18 by lraffin           #+#    #+#             */
-/*   Updated: 2022/03/17 20:23:38 by lraffin          ###   ########.fr       */
+/*   Updated: 2022/04/14 14:41:48 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,11 +34,14 @@ FragTrap::FragTrap(FragTrap const &src) : ClapTrap(src)
 {
 	std::cout << "[\033[31mFRAG\033[0m] ";
 	std::cout << _name << " has been cloned." << std::endl;
+	*this = src;
 }
 
 FragTrap	&FragTrap::operator=(FragTrap const &rhs)
 {
-	*this = rhs;
+	this->_attack = rhs._attack;
+	this->_energy = rhs._energy;
+	this->_hit = rhs._hit;
 	return (*this);
 }
 
