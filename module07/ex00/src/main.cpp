31c66bc7545a2fecc989c4b56c80b453df586100
louisnfr <louis.raffin@gmail.com>
2022-04-27 19:35:24 +0200

    Improved main

--- module07/ex00/src/main.cpp
+++ module07/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lraffin <lraffin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 17:04:36 by lraffin           #+#    #+#             */
-/*   Updated: 2022/03/25 17:50:10 by lraffin          ###   ########.fr       */
+/*   Updated: 2022/04/27 19:34:58 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,32 @@
 
 int	main(void)
 {
-	int	a = 2;
-	int	b = 3;
+	std::cout << "\033[1;33m* INT *\033[0m\n" << std::endl;
+	{
 
-	::swap(a, b);
+		int	a = 2;
+		int	b = 3;
 
-	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
-	std::string c = "chaine1";
-	std::string d = "chaine2";
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		std::cout << "swap" << std::endl;
+		::swap(a, b);
+		std::cout << "a = " << a << ", b = " << b << std::endl << std::endl;
 
-	::swap(c, d);
+		std::cout << "min(a, b) = " <<::min(a, b) << std::endl;
+		std::cout << "max(a, b) = " <<::max(a, b) << std::endl;
+	}
+	std::cout << std::endl << "\033[1;33m* CHAR *\033[0m\n" << std::endl;
+	{
+		std::string c = "zzz";
+		std::string d = "aaa";
 
-	std::cout << "c = " << c << ", d = " << d << std::endl;
-	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+		std::cout << "c = " << c << ", d = " << d << std::endl;
+		std::cout << "swap" << std::endl;
+		::swap(c, d);
+		std::cout << "c = " << c << ", d = " << d << std::endl << std::endl;
 
+		std::cout << "min(c, d) = " << ::min(c, d) << std::endl;
+		std::cout << "max(c, d) = " << ::max(c, d) << std::endl;
+	}
 	return (0);
 }
