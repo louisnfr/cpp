56c7a46e5f73277a2999b75e34b8a2106e0cf003
louisnfr <louis.raffin@gmail.com>
2022-05-03 16:11:10 +0200

    Improve tests

--- module08/ex01/src/main.cpp
+++ module08/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lraffin <lraffin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/31 22:51:17 by lraffin           #+#    #+#             */
-/*   Updated: 2022/04/25 18:31:13 by lraffin          ###   ########.fr       */
+/*   Updated: 2022/05/03 16:10:48 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,24 +17,19 @@ int	main(void)
 	/**************************************************/
 	std::cout << "** SUBJECT TEST **" << std::endl;
 	Span	sp = Span(5);
-	try
-	{
-		sp.addNumber(6);
-		sp.addNumber(3);
-		sp.addNumber(17);
-		sp.addNumber(9);
-		sp.addNumber(11);
-		sp.print();
-		std::cout << sp.shortestSpan() << std::endl;
-		std::cout << sp.longestSpan() << std::endl;
-	}
-	catch (const std::exception &e)
-	{
-		std::cerr << e.what() << std::endl;
-	}
+	
+	sp.addNumber(6);
+	sp.addNumber(3);
+	sp.addNumber(17);
+	sp.addNumber(9);
+	sp.addNumber(11);
+	sp.print();
+	std::cout << sp.shortestSpan() << std::endl;
+	std::cout << sp.longestSpan() << std::endl;
 	/**************************************************/
 	std::cout << std::endl << "** EXCEPTIONS TEST **" << std::endl;
 	Span	sp2 = Span(10);
+
 	try
 	{
 		sp2.addNumber(42);
@@ -67,7 +62,6 @@ int	main(void)
 	Span	large(42000);
 
 	large.addNumbers(42000, 10000);
-	// large.print();
 	std::cout << large.shortestSpan() << std::endl;
 	std::cout << large.longestSpan() << std::endl;
 	return (0);
